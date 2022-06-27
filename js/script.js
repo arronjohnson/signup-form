9553342604c62ac1c8b7dba5fd21da82ad7d8e6d
Arron Johnson <arronjohnson.uk@gmail.com>
2022-06-27 22:39:11 +0100

    Add password comparison to form validation
    The two password fields are compared for equality after a short
    timeout, to allow the user to type.
    
    If the passwords don't match then a custom error message is set, along
    with the standard styling for invalid inputs.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,57 @@
-document.addEventListener("blur", (event) => validateInput(event), true);
+const inputs = document.querySelectorAll("input");
+const passwords = document.querySelectorAll("input[type='password']");
+const inputPwd = document.getElementById("pwd");
+const inputConfirm = document.getElementById("pwd-confirm");
 
-function validateInput(event) {
-  const isValid = event.target.validity.valid;
-  const message = event.target.validationMessage;
-  const spanId = event.target.getAttribute("aria-describedby");
-  const span = spanId ? document.getElementById(spanId) : false;
+let inputTimer;
 
-  if (isValid) {
-    event.target.className = "valid";
-  } else {
-    event.target.className = "error";
+inputs.forEach((el) =>
+  el.addEventListener("blur", (event) => validateInput(event.target), true)
+);
+
+passwords.forEach((el) =>
+  el.addEventListener("input", () => {
+    clearTimeout(inputTimer);
+    inputTimer = setTimeout(validatePasswords, 1000);
+  })
+);
+
+function validateInput(el) {
+  const isValid = el.validity.valid;
+  toggleClass(el, isValid);
+  toggleMessage(el, isValid);
+
+  if (el.id.startsWith("pwd")) {
+    validatePasswords();
+  }
+}
+
+function validatePasswords() {
+  if (inputPwd.value === "" || inputConfirm.value === "") {
+    return false;
   }
 
+  const match = passwordsMatch();
+  const msg = match ? null : "Passwords don't match";
+  passwords.forEach((el) => {
+    toggleClass(el, match);
+    toggleMessage(el, match, msg);
+  });
+}
+
+function passwordsMatch() {
+  return inputPwd.value !== "" && inputPwd.value === inputConfirm.value;
+}
+
+function toggleClass(el, isValid) {
+  el.className = isValid ? "valid" : "error";
+}
+
+function toggleMessage(el, isValid, msg = el.validationMessage) {
+  const spanId = el.getAttribute("aria-describedby");
+  const span = spanId ? document.getElementById(spanId) : false;
+
   if (span) {
-    span.textContent = isValid ? "" : `\u26A0\uFE0E ${message}`;
+    span.textContent = isValid ? "" : `\u26A0\uFE0E ${msg}`;
   }
 }
